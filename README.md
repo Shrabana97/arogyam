c168b119c5ddd3c8da43bb6b554f135de49ca759
shrabana <shrabanasaha97@gmail.com>
2021-03-23 18:04:39 +0530

    v1

---
+++ README.md
@@ -0,0 +1,18 @@
+# arogyam 
+
+[![GitHub issues](https://img.shields.io/github/issues/sudo-rajarshi/arogyam)](https://github.com/sudo-rajarshi/arogyam/issues)
+[![GitHub stars](https://img.shields.io/github/stars/sudo-rajarshi/arogyam)](https://github.com/sudo-rajarshi/arogyam/stargazers)
+[![GitHub license](https://img.shields.io/github/license/sudo-rajarshi/arogyam)](https://github.com/sudo-rajarshi/arogyam/blob/master/LICENSE)
+
+## About
+* An AI integrated website, where the user will get to know whether he or she is infected by viruses or diseases based on their medical documentations.
+* The website is being hosted using Flask.
+* The front-end is being developed using HTML , CSS and Bootstrap.
+* The data,collected from the user is encrypted and is treated as anonymous data for the sake of privacy.
+
+## Run at your own machine:
+### Linux:
+* `git clone https://github.com/sudo-rajarshi/arogyam.git`
+* `cd arogyam`
+* `pip3 install -r requirements.txt`
+* `python3 app.py`
---
+++ app.py
@@ -0,0 +1,139 @@
+from flask import Flask, render_template, request, send_from_directory
+from flask_ngrok import run_with_ngrok
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import os
+import shutil
+import webbrowser
+from threading import Timer
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+
+tf.config.experimental.list_physical_devices()
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+
+if physical_devices != []:
+    print("Using GPU")
+    for i in physical_devices:
+        tf.config.experimental.set_memory_growth(i, True)
+else:
+    print("Using CPU")
+    pass
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+assets_dir = os.path.join(dir_path, 'database')
+if not os.path.exists(assets_dir):
+    os.mkdir(assets_dir)
+else:
+    shutil.rmtree(assets_dir)
+    os.mkdir(assets_dir)
+
+os.mkdir(os.path.join(assets_dir, 'oct'))
+os.mkdir(os.path.join(assets_dir, 'cxr'))
+
+oct_model = load_model(os.path.join(dir_path, 'models', 'oct_model.h5'))
+cxr_model = load_model(os.path.join(dir_path, 'models', 'cxr_model.h5'))
+
+print("***** models loaded")
+
+cnt_oct = 0
+cnt_cxr = 0
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/oct')
+def oct_index():
+    return render_template('oct_index.html')
+
+@app.route('/oct_prediction', methods=['POST'])
+def oct_prediction():
+    global cnt_oct
+    img = request.files['image']
+    file = os.path.join(assets_dir, 'oct', '{}.jpg'.format(cnt_oct))
+    img.save(file)
+    
+    # pre-process
+    img_cv = cv2.imread(file)
+    res = cv2.resize(img_cv, (128,128))
+    cv2.imwrite(file, res)
+
+    # img to array
+    img_arr = cv2.imread(file)
+    img_arr = cv2.resize(img_arr, (128,128))
+    img_arr = img_arr / 255.0
+    img_arr = img_arr.reshape(1, 128,128,3)
+
+    prediction = oct_model.predict(img_arr)
+
+    cnv = round(prediction[0,0], 2)
+    dme = round(prediction[0,1], 2)
+    drusen = round(prediction[0,2], 2)
+    normal = round(prediction[0,3], 2)
+    
+    preds = np.array([cnv*100, dme*100, drusen*100, normal*100])
+    cnt_oct += 1
+    return render_template('oct_prediction.html', data=preds)
+
+@app.route('/cxr')
+def cxr_index():
+    return render_template('cxr_index.html')
+
+@app.route('/cxr_prediction', methods=['POST'])
+def cxr_prediction():
+    global cnt_cxr
+    img = request.files['image']
+    file = os.path.join(assets_dir, 'cxr', '{}.jpg'.format(cnt_cxr))
+    img.save(file)
+    
+    # pre-process
+    img_cv = cv2.imread(file)
+    res = cv2.resize(img_cv, (128,128))
+    cv2.imwrite(file, res)
+
+    # img to array
+    img_arr = cv2.imread(file)
+    img_arr = cv2.resize(img_arr, (128,128))
+    img_arr = img_arr / 255.0
+    img_arr = img_arr.reshape(1, 128,128,3)
+
+    prediction = cxr_model.predict(img_arr)
+
+    pneumonia = round(prediction[0,0], 2)
+    covid = round(prediction[0,1], 2)
+    normal = round(prediction[0,2], 2)
+    
+    preds = np.array([int(pneumonia*100), int(covid*100), int(normal*100)])
+    cnt_cxr += 1
+    return render_template('cxr_prediction.html', data=preds)
+
+
+@app.route('/load_img_oct')
+def load_img_oct():
+    global cnt_oct
+    return send_from_directory(os.path.join(assets_dir,'oct'),'{}.jpg'.format(cnt_oct-1))
+
+@app.route('/load_img_cxr')
+def load_img_cxr():
+    global cnt_cxr
+    return send_from_directory(os.path.join(assets_dir,'cxr'),'{}.jpg'.format(cnt_cxr-1))
+
+def open_browser():
+      webbrowser.open_new('http://127.0.0.1:5000/')
+
+
+if __name__ == '__main__':
+    Timer(1, open_browser).start();
+    app.run()
+
+
+
+
